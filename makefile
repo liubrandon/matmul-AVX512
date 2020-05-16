681f10807b9527df677ef95729622205472f94d8
Brandon Liu <byliu88@gmail.com>
2020-05-15 23:49:06 -0400

    Added generateMatrix, print __mm512i, and multiply

---
+++ makefile
@@ -0,0 +1,13 @@
+# Macro definitions (which flags are necessary?)
+CXXFLAGS = -std=c++11 -w -O3 -march=native -g -larmadillo -Wl,--no-as-needed -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl -Wall
+OBJ = matmul_avx512.o
+TARGET = matmul
+
+# Rules
+all: $(TARGET)
+$(TARGET): $(OBJ)
+		$(CXX) -o $@ $(OBJ)
+clean:
+		rm -f $(OBJ) $(TARGET) *.out vgcore.*
+
+# Dependencies
---
+++ matmul_avx512.cpp
@@ -0,0 +1,60 @@
+#include "immintrin.h"
+#include <cstdio>
+#include <iostream>
+#include <iomanip>
+#include <cassert>
+using namespace std;
+
+struct Complex {
+ int16_t real;
+ int16_t image;
+};
+
+// Takes the 512 bit vector of integers vec (__m512i ) and
+// prints the 32 short ints (16 bits each) stored inside
+void print512_num(__m512i vec) {
+    int16_t* val = (int16_t*)&vec;
+    cout << "__m512i: ";
+    for(int i = 0; i < 32; i++) {
+        cout << setw(4) << val[i];
+    }
+    cout << endl;
+}
+
+// Fills matrix incrementally from 0 - size,
+// where size = # rows * # columns
+// if mode is 0 then fill with zeros
+void generateMatrix(int16_t* mat, int size, int mode=1) {
+    for(int i = 0; i < size; i++) {
+        mat[i] = mode ? i : 0;
+    }
+}
+
+// Matrix multiplication AB = C
+// where matrix A has dimensions r1 x c1 and B is dimensions c1 x c2 
+void matmul_AVX512(const int16_t* A, int r1, int c1, const int16_t* B, int c2, int16_t* C) {
+    __m512i* a_row = (__m512i*)A;
+    __m512i* b_col = (__m512i*)B;
+    print512_num(*a_row);
+    print512_num(*b_col);
+    //for(int i = 0; i < r1; i+=32) {
+        __m512i dp;
+        // multiplies ints in each of the two vectors and saves the low bits in dp
+        dp = _mm512_mullo_epi16(*a_row, *b_col);
+    //}
+    print512_num(dp);
+    
+}
+
+int main() {
+    int r1 = 32;  // Treat A as a 32 x 32 matrix
+    int c1 = 32; 
+    int16_t A[r1*c1]__attribute__( ( aligned (64) ) ); // what to align it to?
+    int16_t B[c1]__attribute__( ( aligned (64) ) ); // B is a 32 x 1 matrix (vector)
+    int16_t C[c1]__attribute__( ( aligned (64) ) ); // C is the 32 x 1 resulting matrix
+    generateMatrix(&A[0], r1*c1);
+    generateMatrix(&B[0], c1);
+    generateMatrix(&C[0], c1, 0); // initialize C to all 0s
+    matmul_AVX512(&A[0], r1, c1, &B[0], 1, &C[0]);
+    return 0;
+}
