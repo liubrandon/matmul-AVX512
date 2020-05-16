55c5bb3ea3af3dc9bec2b5866bc1c39b71dd92f2
Brandon Liu <byliu88@gmail.com>
2020-05-16 00:01:45 -0400

    Small formatting changes

--- matmul_avx512.cpp
+++ matmul_avx512.cpp
@@ -12,7 +12,7 @@ struct Complex {
 
 // Takes the 512 bit vector of integers vec (__m512i ) and
 // prints the 32 short ints (16 bits each) stored inside
-void print512_num(__m512i vec) {
+void print_m512i(__m512i vec) {
     int16_t* val = (int16_t*)&vec;
     cout << "__m512i: ";
     for(int i = 0; i < 32; i++) {
@@ -32,17 +32,17 @@ void generateMatrix(int16_t* mat, int size, int mode=1) {
 
 // Matrix multiplication AB = C
 // where matrix A has dimensions r1 x c1 and B is dimensions c1 x c2 
-void matmul_AVX512(const int16_t* A, int r1, int c1, const int16_t* B, int c2, int16_t* C) {
+void matmulAVX512(const int16_t* A, int r1, int c1, const int16_t* B, int c2, int16_t* C) {
     __m512i* a_row = (__m512i*)A;
     __m512i* b_col = (__m512i*)B;
-    print512_num(*a_row);
-    print512_num(*b_col);
+    print_m512i(*a_row);
+    print_m512i(*b_col);
     //for(int i = 0; i < r1; i+=32) {
         __m512i dp;
         // multiplies ints in each of the two vectors and saves the low bits in dp
         dp = _mm512_mullo_epi16(*a_row, *b_col);
     //}
-    print512_num(dp);
+    print_m512i(dp);
     
 }
 
@@ -55,6 +55,6 @@ int main() {
     generateMatrix(&A[0], r1*c1);
     generateMatrix(&B[0], c1);
     generateMatrix(&C[0], c1, 0); // initialize C to all 0s
-    matmul_AVX512(&A[0], r1, c1, &B[0], 1, &C[0]);
+    matmulAVX512(&A[0], r1, c1, &B[0], 1, &C[0]);
     return 0;
 }
