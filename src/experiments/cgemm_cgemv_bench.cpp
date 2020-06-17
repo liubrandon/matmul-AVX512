e1812f468892f4c09d0c9047ae99c6270f7b6335
Brandon Liu <byliu88@gmail.com>
2020-06-17 19:50:19 -0400

    Uplink/downlink benchmarks and csv output added

--- src/experiments/cgemm_cgemv_bench.cpp
+++ src/experiments/cgemm_cgemv_bench.cpp
@@ -1,20 +1,25 @@
 #include <mkl.h>
 #include <cstdio>
 #include <iostream>
+#include <fstream>
 #include <iomanip>
 #include <armadillo>
 #include <cassert>
+#include <string>
 #include "../timer.hpp"
 
 #define WARMUP 100
-#define NITER 1000000
+#define NITER 100000
 #define TOLERANCE 0.0001f
+
+#define RESET   "\033[0m" // Terminal color codes
+#define BOLDGREEN   "\033[1m\033[32m" 
+
 // m x n matrix
 double benchArma(arma::cx_fmat& a1, arma::cx_fmat& x1, arma::cx_fmat& y1, int m, int n, int numIter) {
     double start = getTime();
-    for(int i = 0; i < numIter; i++) {
+    for(int i = 0; i < numIter; i++)
         y1 = a1 * x1;
-    }
     return timeSince(start);
 }
 
@@ -24,9 +29,8 @@ double benchCGEMV(MKL_Complex8* a, MKL_Complex8* x, MKL_Complex8* y, MKL_INT m, 
     MKL_Complex8 beta = {0, 0};
     MKL_INT lda = m;
     double start = getTime();
-    for(int i = 0; i < numIter; i++) {
+    for(int i = 0; i < numIter; i++)
         cblas_cgemv(CblasColMajor, CblasNoTrans, m, n, &alpha, a, lda, x, 1, &beta, y, 1);
-    }
     return timeSince(start);
 }
 
@@ -38,13 +42,11 @@ double benchCGEMM(MKL_Complex8* a, MKL_Complex8* b, MKL_Complex8* c, MKL_INT m, 
     MKL_INT ldb = k;
     MKL_INT ldc = m;
     double start = getTime();
-    for(int i = 0; i < numIter; i++) {
-        // a is m x k, b is k x n, c is m x n
+    for(int i = 0; i < numIter; i++) // a is m x k, b is k x n, c is m x n
         cblas_cgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, m, 1, k, &alpha, a, lda, b, ldb, &beta, c, ldc);
-    }
     return timeSince(start);
 }
-
+// m x k matrix
 double benchJITCGEMM(MKL_Complex8* a, MKL_Complex8* b, MKL_Complex8* c, MKL_INT m, MKL_INT k, int numIter) {
     if(numIter == 0) return 0.0;
     MKL_Complex8 alpha = {1, 0};
@@ -60,9 +62,8 @@ double benchJITCGEMM(MKL_Complex8* a, MKL_Complex8* b, MKL_Complex8* c, MKL_INT 
         exit(1);
     }
     cgemm_jit_kernel_t my_cgemm = mkl_jit_get_cgemm_ptr(jitter);
-    for(int i = 0; i < numIter; i++) {
+    for(int i = 0; i < numIter; i++)
         my_cgemm(jitter, a, b, c);
-    }
     return timeSince(start);
 }
 
@@ -74,6 +75,7 @@ MKL_Complex8 accum(MKL_Complex8* a, int size) {
     }
     return ret;
 }
+
 bool cmpf(arma::cx_float a, MKL_Complex8 b, float epsilon = TOLERANCE) {
     return (fabs(a.real() - b.real) < epsilon) && (fabs(a.imag() - b.imag) < epsilon);
 }
@@ -85,39 +87,35 @@ bool vectorsEqual(arma::cx_fmat& src, MKL_Complex8* a, MKL_Complex8* b, MKL_Comp
     }
     return true;
 }
+
 void showUsageAndExit(char* progName) {
-    fprintf(stderr, "Usage: %s [MxN] [-v] [numIter]\nRuns different MKL functions for matrix-vector multiply numIter times with an MxN matrix.\nIf you enable MKL verbose mode with -v numIter will be set to 1 and subsequent arguments will be ignored.\n", progName);
+    fprintf(stderr, "Usage: %s [MxN] [-v] [numIter] [--uplink|--downlink]\n       "
+                    "Benchmarks different implementations for matrix-vector multiply, running numIter times with an MxN matrix. (for MU-MIMO linear precoding)\n       "
+                    "If you enable MKL verbose mode with -v numIter will be set to 1 and subsequent arguments will be ignored.\n       "
+                    "--uplink runs tests on matrices from size M x N to 1 x N\n       "
+                    "--downlink runs tests on matrices from size M x N to M x 1\n", progName);
     exit(1);
 }
 
-int main(int argc, char *argv[]) {
-    srand(time(0));
-    unsigned long warmup = WARMUP;
-    unsigned long numIter = NITER;
-    MKL_INT m = 64;
-    MKL_INT n = 64;
-    bool dimSet = false;
-    if(argc > 4)
-        showUsageAndExit(argv[0]);
-    if(argc > 1) {
-        for(int i = 1; i < argc; i++) {
-            if(strcmp(argv[i], "-v") == 0) {
-                warmup = 0;
-                numIter = 1;
-                setenv("MKL_VERBOSE", "1", 1);
-                break;
-            } else if(!dimSet && strtoul(argv[i], NULL, 0) != 0) {
-                char* nPtr;
-                m = strtoul(argv[i], &nPtr, 0);
-                n = strtoul(nPtr+1, NULL, 0);
-                dimSet = true;
-            } else if(strtoul(argv[i], NULL, 0) != 0) {
-                numIter = (strtoul(argv[i], NULL, 0));
-            } else {
-                showUsageAndExit(argv[0]);
-            }
-        }
-    }
+typedef enum {
+    BENCH_SINGLE,
+    BENCH_UPLINK,  // (N x 64) * (64 x 1)
+    BENCH_DOWNLINK//  (64 x N) * (N x 1)
+} BENCH_MODE;
+
+std::vector<std::string> dimensions;
+std::vector<double> armaTimes, cgemvTimes, cgemmTimes, jitcgemmTimes;
+void outputCSV() {
+    std::ofstream outFile;
+    outFile.open("results.csv");
+    outFile << "Dimensions,Armadillo cgemv,cgemv,cgemm,JIT cgemm,\n";
+    for(int i = 0; i < dimensions.size(); i++)
+        outFile << dimensions[i] << "," << armaTimes[i] << "," << cgemvTimes[i] << "," << cgemmTimes[i] << "," << jitcgemmTimes[i] << ",\n";
+    outFile.close();
+}
+
+
+void runMKLBenchmarks(unsigned long warmup, unsigned long numIter, MKL_INT m, MKL_INT n, bool quiet = false) {
     MKL_Complex8 *a, *x, *y, *a2, *b2, *c2, *a3, *b3, *c3;
     arma::cx_fmat a1(m, n);
     arma::cx_fmat x1(n, 1);
@@ -137,7 +135,7 @@ int main(int argc, char *argv[]) {
     assert((a!=NULL)&&(x!=NULL)&&(y!=NULL)&&
            (a2!=NULL)&&(b2!=NULL)&&(c2!=NULL)&&
            (a3!=NULL)&&(b3!=NULL)&&(c3!=NULL)); // make sure mkl_calloc didn't fail
-    
+
     // Generate the same random matrix and vector for multiplication for all benchmarks
     for(int i = 0; i < m*n; i++) {
         a[i] = {static_cast<float>(rand()/static_cast<float>(RAND_MAX)), static_cast<float>(rand()/static_cast<float>(RAND_MAX))};
@@ -151,30 +149,101 @@ int main(int argc, char *argv[]) {
         b2[i] = {x[i].real, x[i].imag};
         b3[i] = {x[i].real, x[i].imag};
     }
+    // Warmup before benching
     benchArma(a1, x1, y1, m, n, warmup);
     benchCGEMV(a, x, y, m, n, warmup);
     benchCGEMM(a, x, y, m, n, warmup);
     benchJITCGEMM(a, x, y, m, n, warmup);
+
+    // Bench MKL and output average time per iteration
     double armaTime = benchArma(a1, x1, y1, m, n, numIter);
     double cgemvTime = benchCGEMV(a, x, y, m, n, numIter);
     double cgemmTime = benchCGEMM(a2, b2, c2, m, n, numIter);
     double jitcgemmTime = benchJITCGEMM(a3, b3, c3, m, n, numIter);
-    printf("%ld iterations, (%dx%d) * (%dx%d)\n", numIter, m, n, n, 1);
+    printf("\n        ---------- \n\n");
+    printf("     %ld iterations, (%dx%d) * (%dx%d)\n", numIter, m, n, n, 1);
     printf("Armadillo cgemv: %.5f µs per iteration\n", armaTime/(double)numIter);
     printf("    cblas_cgemv: %.5f µs per iteration\n", cgemvTime/(double)numIter);
     printf("    cblas_cgemm: %.5f µs per iteration\n", cgemmTime/(double)numIter);
     printf("  mkl_jit_cgemm: %.5f µs per iteration\n", jitcgemmTime/(double)numIter);
+
+    // Output JIT speed-up
+    std::cout << "  " << BOLDGREEN << std::fixed << std::setprecision(2) << armaTime/jitcgemmTime << "x" << RESET << " Armadillo wrapped cgemv" << std::endl;
+    std::cout << "  " << BOLDGREEN << std::fixed << std::setprecision(2) << cgemvTime/jitcgemmTime << "x" << RESET << " cgemv called directly" << std::endl;
     // Assert resulting values are within TOLERANCE of each other
     assert(vectorsEqual(y1, y, c2, c3));
-    printf("\nResulting vector values are within %.4f of the result calculated by Armadillo\n", TOLERANCE);
-    // Output sums of resulting vectors to make sure they're the same
-    printf("Accumulation of resulting vectors\n");
-    printf("Armadillo cgemv: (%.4f,%.4f)\n", arma::accu(y1).real(), arma::accu(y1).imag());
-    printf("    cblas_cgemv: (%.4f,%.4f)\n",accum(y, m).real, accum(y, m).imag);
-    printf("    cblas_cgemm: (%.4f,%.4f)\n", accum(c2, m).real, accum(c2, m).imag);
-    printf("  mkl_jit_cgemm: (%.4f,%.4f)\n", accum(c3, m).real, accum(c3, m).imag);
+    if(!quiet) {
+        printf("Individual values are within %.4f of the results calculated by Armadillo\n", TOLERANCE);
+        // Output sums of resulting vectors to make sure they're the same
+        printf("\nAccumulation of resulting vectors\n");
+        printf("Armadillo cgemv: (%.4f,%.4f)\n", arma::accu(y1).real(), arma::accu(y1).imag());
+        printf("    cblas_cgemv: (%.4f,%.4f)\n", accum(y, m).real, accum(y, m).imag);
+        printf("    cblas_cgemm: (%.4f,%.4f)\n", accum(c2, m).real, accum(c2, m).imag);
+        printf("  mkl_jit_cgemm: (%.4f,%.4f)\n", accum(c3, m).real, accum(c3, m).imag);
+    }
+
+    // Save results in global vectors to later output as csv
+    dimensions.push_back(std::to_string(m) + "x" + std::to_string(n));
+    armaTimes.push_back(armaTime/(double)numIter);
+    cgemvTimes.push_back(cgemvTime/(double)numIter);
+    cgemmTimes.push_back(cgemmTime/(double)numIter);
+    jitcgemmTimes.push_back(jitcgemmTime/(double)numIter);
 
     mkl_free(a); mkl_free(x); mkl_free(y);
     mkl_free(a2); mkl_free(b2); mkl_free(c2);
     mkl_free(a3); mkl_free(b3); mkl_free(c3);
+}
+
+
+int main(int argc, char *argv[]) {
+    srand(time(0));
+    unsigned long warmup = WARMUP;
+    unsigned long numIter = NITER;
+    MKL_INT m = 64;
+    MKL_INT n = 64;
+    BENCH_MODE mode = BENCH_SINGLE;
+    bool dimSet = false;
+    if(argc > 4 || argc == 1)
+        showUsageAndExit(argv[0]);
+    if(argc > 1) {
+        for(int i = 1; i < argc; i++) {
+            if(strcmp(argv[i], "-v") == 0) {
+                warmup = 0;
+                numIter = 1;
+                setenv("MKL_VERBOSE", "1", 1);
+                break;
+            } else if(!dimSet && strtoul(argv[i], NULL, 0) != 0) {
+                char* nPtr;
+                m = strtoul(argv[i], &nPtr, 0);
+                n = strtoul(nPtr+1, NULL, 0);
+                dimSet = true;
+            } else if(strtoul(argv[i], NULL, 0) != 0) {
+                numIter = (strtoul(argv[i], NULL, 0));
+            } else if(strcmp(argv[i], "--uplink") == 0) {
+                mode = BENCH_UPLINK;
+            } else if(strcmp(argv[i], "--downlink") == 0) {
+                mode = BENCH_DOWNLINK;
+            } else {
+                showUsageAndExit(argv[0]);
+            }
+        }
+    }
+    switch (mode) {
+        case BENCH_SINGLE:
+            runMKLBenchmarks(warmup, numIter, m, n);
+            break;
+        case BENCH_UPLINK:
+            for(int i = m; i > 0; i--)
+                runMKLBenchmarks(warmup, numIter, i, n, 1);
+            outputCSV();
+            break;
+        case BENCH_DOWNLINK:
+            for(int i = n; i > 0; i--)
+                runMKLBenchmarks(warmup, numIter, m, i, 1);
+            outputCSV();
+            break;
+        default:
+            fprintf(stderr, "Should not get here\n");
+            break;
+    }
 }
